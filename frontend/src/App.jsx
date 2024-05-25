5c6f45fff5cc87f96d194d9d424d7bc549295b53
Giimyy <sergiulive2007@yahoo.com>
2024-05-25 21:46:43 +0300

    #2 -project setup(folders, files, tailwind components)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Layout from "./layout/Layout";
 
 function App() {
-  return (
-    <>
-      <h1>MedVox App</h1>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
